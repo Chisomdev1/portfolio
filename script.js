7105527629b0b52df10367537c60e816df9f7537
chisomdev1 <chisomchidike03@gmail.com>
2025-02-22 07:16:23 -1000

    cooking

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ document.querySelectorAll('.faq-question').forEach(item => {
       faqItem.classList.toggle('active');
   });
 });
+
+document.querySelectorAll('.nav-link').forEach(item => {
+  item.addEventListener('click', () => {
+      document.querySelector('.navbar-collapse').classList.remove('show');
+  });
+});
+
+document.getElementById('navbarToggler').addEventListener('click', () => {
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+  if (navbarCollapse.classList.contains('show')) {
+      navbarCollapse.classList.remove('show');
+  } else {
+      navbarCollapse.classList.add('show');
+  }
+});
